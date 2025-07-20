3ce2419c60f1122b65fabb880a44e6ac5e2c3ae0
JosephLongo1 <rossiji@bc.edu>
2025-07-19 21:21:35 -0400

    bone

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-let size = 0;
+document.addEventListener("DOMContentLoaded", function() {
+    let score = 0;
+    let score_increment = 1
+    const score_box = document.getElementById("score_box");
+    let score_box_showing = false;
 
-function summon() {
-    size += 1;
-    console.log(size);
-};
+    const skellington = document.getElementById("skellington");
+    skellington.addEventListener("click", summon);
+
+    const zeroZero = document.getElementById("zeroZero");
+    let rect = zeroZero.getBoundingClientRect();
+    let midpoint_x = (rect.x + rect.x + rect.width) / 2;
+
+    function summon() {
+        score += score_increment;
+        updateScoreBox();
+        let bone = document.createElement("img");
+        bone.setAttribute("src", "Assets/bone.png")
+        zeroZero.appendChild(bone)
+    };
+
+    function updateScoreBox() {
+        if (!score_box_showing) {
+            score_box.className = "scoreBox";
+            score_box_showing = true;
+        }
+        score_box.innerText = `You have summoned ${score} bones`;
+    }
+
+}
+)
+
+
+
+
